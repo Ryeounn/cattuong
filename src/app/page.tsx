584a3c573f6f29359622a69537b0285bd73c39f8
Ryeounn <ryeoun.eun@gmail.com>
2025-02-25 15:18:13 +0700

    1.0.0.7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 import { useState } from "react";
-import { faTruck, faCircleCheck, faEnvelope, faFaceKissWinkHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
 import Banner from "./banner/banner";
 import Benefit from "./benefit/benefit";
-import Product from "./products/product";
-import ContactFor from "./contact/contact";
 import Nav from "./nav/nav";
 
 export default function Home() {
